65287b9241a1aa065fe5d3845d55ba81f0610508
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-16 16:29:40 -0700

    Set up trap handler and added a method that enables interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
-#include "../h/MemoryAllocator.h"
+#include "../h/BitMasks.h"
+#include "../h/RegisterUtils.h"
+#include "../h/TrapHandles.h"
 
-int main() {
+void enableInterrupts() {
+    using namespace kernel::BitMasks;
+    SYS_REGISTER_SET_BITS(sstatus, sstatus::SIE);
+}
+
+void main() {
+    // Set supervisorTrap as a trap handler
+    WRITE_TO_SYS_REGISTER(&supervisorTrap, stvec);
+
+    //enableInterrupts();
+
+    void userMain();
+    userMain();
 }
 
