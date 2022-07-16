44d4e7dfaa54014503a09eb0a7ecd20467b0834e
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-16 15:34:06 -0700

    Renamed MemoryAllocator::freeBlocks to MemoryAllocator::deallocateBlocks Moved MemoryAllocator to kernel namespace

--- h/MemoryAllocator.h
+++ h/MemoryAllocator.h
@@ -5,25 +5,23 @@
 #define PROJECT_MEMORYALLOCATOR_H
 
 #include "../lib/hw.h"
-
-class MemoryAllocator {
-private:
-    struct FreeMemoryBlock{
-        FreeMemoryBlock *prev, *next;
-        size_t size;
+namespace kernel {
+    class MemoryAllocator {
+    private:
+        struct FreeMemoryBlock{
+            FreeMemoryBlock *prev, *next;
+            size_t size;
+        };
+        FreeMemoryBlock* freeMemoryHead;
+    public:
+        MemoryAllocator();
+        MemoryAllocator(MemoryAllocator const&)=delete;
+        MemoryAllocator& operator=(MemoryAllocator const&)=delete;
+        static MemoryAllocator& getInstance();
+        static size_t byteSizeToBlockCount(size_t blocks);
+        void* allocateBlocks(size_t blockCount);
+        int deallocateBlocks(void* address);
     };
-    FreeMemoryBlock* freeMemoryHead;
-public:
-    MemoryAllocator();
-    MemoryAllocator(MemoryAllocator const&)=delete;
-    MemoryAllocator(MemoryAllocator&&) =delete;
-    MemoryAllocator& operator=(MemoryAllocator const&)=delete;
-    MemoryAllocator& operator=(MemoryAllocator&&)=delete;
-    static MemoryAllocator& getInstance();
-    static size_t byteSizeToBlockCount(size_t blocks);
-    void* allocateBlocks(size_t blockCount);
-    int freeBlocks(void* address);
-};
-
+}
 
 #endif //PROJECT_MEMORYALLOCATOR_H
