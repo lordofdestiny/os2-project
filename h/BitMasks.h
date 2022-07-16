b3f497d0f4b3a88bb2a540042de3559dba4ae2f4
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-16 16:26:20 -0700

    Written utility function in order to remove code repetition of defining masks

--- h/BitMasks.h
+++ h/BitMasks.h
@@ -6,35 +6,29 @@
 #define PROJECT_BITMASKS_H
 #include "../lib/hw.h"
 
+static constexpr size_t maskBit(uint8 bit) {
+    return (size_t) 0x01 << (size_t) bit;
+}
+
 namespace kernel {
     namespace BitMasks {
         enum class sstatus : size_t {
-            SPP = (size_t) 0x01 << 8,
-            SPIE = (size_t) 0x01 << 5,
-            SIE = (size_t) 0x01<< 1,
+            SPP = maskBit(8),
+            SPIE = maskBit(5),
+            SIE = maskBit(1),
         };
         enum class scause : size_t{
-            BNT = (size_t) 0x01 << 63
+            BNT = maskBit(63)
         };
         enum class sip : size_t {
-            SEIP = (size_t) 0x01 << 9,
-            SSIP = 0x01 << 0x01,
+            SEIP = maskBit(9),
+            SSIP = maskBit(1),
         };
         enum class sie : size_t {
-            SEIE = (size_t) 0x01 << 9,
-            SSIE = (size_t) 0x01 << 1
+            SEIE = maskBit(9),
+            SSIE = maskBit(1),
         };
     }
-
-    enum class TrapType :size_t {
-        TimerTrap = (size_t) BitMasks::scause::BNT | 0x01,
-        ExternalHardwareTrap = (size_t) BitMasks::scause::BNT | (size_t) 0x09,
-        IllegalInstruction = 0x02,
-        IllegalReadAddress = 0x05,
-        IllegalWriteAddress = 0x07,
-        UserSystemCall = 0x08,
-        SystemSystemCall = 0x09
-    };
 }
 
 
