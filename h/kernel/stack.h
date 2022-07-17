948c460bb48d9e4dfcc8ced8bf62cdaa16c50079
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-17 06:52:42 -0700

    Declared and defined systemStack with alignment to 16 bytes

---
+++ h/kernel/stack.h
@@ -0,0 +1,12 @@
+//
+// Created by os on 7/17/22.
+//
+
+#ifndef PROJECT_STACK_H
+#define PROJECT_STACK_H
+
+#include "../../lib/hw.h"
+
+extern uint8 systemStack[DEFAULT_STACK_SIZE];
+
+#endif //PROJECT_STACK_H
