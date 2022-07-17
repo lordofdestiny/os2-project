fde5561b118c2fb37fbfd350e599e2707344c627
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-17 03:09:32 -0700

    Class that will collect allocated resources at the end

---
+++ h/kernel/Collector.h
@@ -0,0 +1,16 @@
+//
+// Created by os on 7/17/22.
+//
+
+#ifndef PROJECT_COLLECTOR_H
+#define PROJECT_COLLECTOR_H
+
+namespace kernel {
+    class Collector {
+    public:
+        static void cleanup();
+    };
+}
+
+
+#endif //PROJECT_COLLECTOR_H
---
+++ src/kernel/Collector.cpp
@@ -0,0 +1,9 @@
+//
+// Created by os on 7/17/22.
+//
+
+#include "../../h/kernel/Collector.h"
+
+void kernel::Collector::cleanup() {
+
+}
