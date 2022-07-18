40bebba6527631a2cb8bb227c38a400db23c8ca4
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-18 07:34:55 -0700

    Added thread initialization function to the start of the main function Tested synchronous context switching

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include "../h/kernel/RegisterUtils.h"
 #include "../h/kernel/TrapHandler.h"
 #include "../h/kernel/Collector.h"
+#include "../h/kernel/TCB.h"
+#include "../h/syscall_c.h"
+#include "../lib/console.h"
+#include "../h/kernel/ConsoleUtils.h"
 
 void enableInterrupts() {
     using namespace kernel::BitMasks;
@@ -19,6 +23,27 @@ void main() {
     WRITE_TO_SYS_REGISTER(&TrapHandler::supervisorTrap, stvec);
 
     //enableInterrupts();
+
+    kernel::TCB::initializeMainThread();
+
+    thread_t thread1;
+    thread_create(&thread1,[](void*){
+        while(true) {
+            printString("Hello from A!\n");
+            thread_dispatch();
+        }
+    }, nullptr);
+
+
+    thread_t thread2;
+    thread_create(&thread2,[](void*){
+        while(true) {
+            printString("Hello from B!\n");
+            thread_dispatch();
+        }
+    }, nullptr);
+
+    while(true) thread_dispatch();
 
     void userMain();
     userMain();
