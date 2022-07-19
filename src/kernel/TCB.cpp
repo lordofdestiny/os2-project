d3b874504624d6a18fdecde634dee309137b1f13
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-18 19:13:14 -0700

    Code style chanegs

--- src/kernel/TCB.cpp
+++ src/kernel/TCB.cpp
@@ -14,6 +14,50 @@ namespace kernel {
     TCB* TCB::mainThread;
     uint64 TCB::threadIdSource = 0;
 
+    void* TCB::operator new(size_t size) {
+        auto blocks = MemoryAllocator::byteSizeToBlockCount(size);
+        return MemoryAllocator::getInstance().allocateBlocks(blocks);
+    }
+
+    void TCB::operator delete(void *ptr) noexcept {
+        MemoryAllocator::getInstance().deallocateBlocks(ptr);
+    }
+
+    void TCB::initializeMainThread() {
+        mainThread = new TCB(nullptr, nullptr, nullptr, ThreadType::USER);
+        runningThread = mainThread;
+        runningTimeLeft = DEFAULT_TIME_SLICE;
+    }
+
+    void TCB::tick() {
+        return;
+    }
+
+    void TCB::dispatch() {
+        auto& scheduler = Scheduler::getInstance();
+
+        auto oldThread = runningThread;
+
+        if (oldThread && oldThread->status != ThreadStatus::SLEEPING) {
+            scheduler.put(oldThread);
+        }
+
+        auto newThread = scheduler.get();
+
+        runningThread = newThread;
+        runningThread->status = ThreadStatus::RUNNING;
+        runningTimeLeft = DEFAULT_TIME_SLICE;
+    }
+
+    TCB *TCB::getRunningThread() {
+        return runningThread;
+    }
+
+    void TCB::taskWrapper() {
+        runningThread->task(runningThread->arg);
+        thread_exit();
+    }
+
     uint64 TCB::sstatusGetInitial() {
         if(runningThread == nullptr) return 0;
         auto mask = (uint64)BitMasks::sstatus::SPP | (uint64) BitMasks::sstatus::SPIE;
@@ -46,33 +90,6 @@ namespace kernel {
         }
     }
 
-    TCB::ThreadContext::ThreadContext(uint64 status, uint64* pc) :
-            programCounter(pc), sstatus(status), registers(){ }
-
-    TCB::Registers::Registers() {
-        for(int i = 0; i < 32; i++){
-            ((uint64*)this)[i] = 0;
-        }
-    }
-
-    void TCB::taskWrapper() {
-        runningThread->task(runningThread->arg);
-        thread_exit();
-    }
-
-    void* TCB::operator new(size_t size) {
-        auto blocks = MemoryAllocator::byteSizeToBlockCount(size);
-        return MemoryAllocator::getInstance().allocateBlocks(blocks);
-    }
-
-    void TCB::operator delete(void *ptr) noexcept {
-        MemoryAllocator::getInstance().deallocateBlocks(ptr);
-    }
-
-    TCB *TCB::getRunningThread() {
-        return runningThread;
-    }
-
     TCB::~TCB() {
         if(stack != nullptr) {
             MemoryAllocator::getInstance().deallocateBlocks(stack);
@@ -83,59 +100,13 @@ namespace kernel {
         }
     }
 
-    void TCB::initializeMainThread() {
-        mainThread = new TCB(nullptr, nullptr, nullptr, ThreadType::USER);
-        runningThread = mainThread;
-        runningTimeLeft = DEFAULT_TIME_SLICE;
-    }
+    TCB::ThreadContext::ThreadContext(uint64 status, uint64* pc) :
+            pc(pc), sstatus(status), registers(){ }
 
-    void TCB::tick() {
-        return;
-    }
-
-    void TCB::dispatch() {
-        auto& scheduler = Scheduler::getInstance();
-
-        auto oldThread = runningThread;
-
-        if (oldThread && oldThread->status != ThreadStatus::SLEEPING) {
-            scheduler.put(oldThread);
+    TCB::Registers::Registers() {
+        for(int i = 0; i < 32; i++){
+            ((uint64*)this)[i] = 0;
         }
-
-        auto newThread = scheduler.get();
-
-        runningThread = newThread;
-        runningThread->status = ThreadStatus::RUNNING;
-        runningTimeLeft = DEFAULT_TIME_SLICE;
     }
 
-    uint64 TCB::getThreadId() const {
-        return id;
-    }
-
-    TCB::Registers& TCB::getRegisters() {
-        return context.registers;
-    }
-
-    uint64* TCB::getPC() const {
-        return context.programCounter;
-    }
-    void TCB::setPC(uint64* value) {
-        context.programCounter = value;
-    }
-
-    uint64 TCB::getsstatus() const {
-        return context.sstatus;
-    }
-    void TCB::setStatus(ThreadStatus newStatus) {
-        status = newStatus;
-    }
-    
-    TCB::ThreadStatus TCB::getStatus() {
-        return status;
-    }
-
-    bool TCB::isUserThread() const{
-        return type == ThreadType::USER;
-    }
 } // kernel
