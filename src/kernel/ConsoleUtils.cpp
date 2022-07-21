cf9588a3bdfbb05493fe4997157566c6839ce1e1
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-21 11:26:05 -0700

    Small cosmetic changes to the code

--- src/kernel/ConsoleUtils.cpp
+++ src/kernel/ConsoleUtils.cpp
@@ -4,9 +4,9 @@
 #include "../../h/kernel/ConsoleUtils.h"
 #include "../../lib/console.h"
 
-void printString(const char* text) {
-    auto ptr = (char*)text;
-    while(*ptr != '\0') __putc(*ptr++);
+void printString(const char *text) {
+    auto ptr = (char *) text;
+    while (*ptr != '\0') __putc(*ptr++);
 }
 
 void printHex(uint64 num) {
@@ -16,21 +16,21 @@ void printHex(uint64 num) {
 
     auto digits = "0123456789ABCDEF";
 
-    for(int i =0; i < 16; i++) {
+    for (int i = 0; i < 16; i++) {
         uint64 cdigit = num % 16;
-        str[17-i] = digits[cdigit];
+        str[17 - i] = digits[cdigit];
         num >>= 4;
     }
 
     str[18] = '\n';
     str[19] = '\0';
 
-    for(auto i = 0 ; i < 19; i++){
+    for (auto i = 0; i < 19; i++) {
         __putc(str[i]);
     }
 }
 
-void printReg(const char* name, uint64 value) {
+void printReg(const char *name, uint64 value) {
     printString(name);
     printString(": ");
     printHex(value);
