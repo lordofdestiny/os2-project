ba13f60a667b8dac274bb2d8560cdc592b785987
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-21 15:25:31 -0700

    Implemented console class. Currently, there has to be input for the output to show

--- h/kernel/Console.h
+++ h/kernel/Console.h
@@ -5,10 +5,37 @@
 #ifndef PROJECT_CONSOLE_H
 #define PROJECT_CONSOLE_H
 
+#include "Semaphore.h"
+#include "BoundedBuffer.h"
+#include "ConsoleController.h"
+#include "../syscall_c.h"
+
 namespace kernel {
+    class Console final{
+    public:
+        Console() {
+        }
+        Console(Console const&)=delete;
+        Console& operator=(Console const&)=delete;
+        ~Console()=default;
 
-    class Console {
+        enum class Direction {
+            IN, OUT
+        };
 
+        sem_t getSemaphore(Direction direction);
+
+        char readChar();
+        void writeChar(char c);
+        void handle();
+    public:
+        static Console& getInstance();
+
+        static const size_t BufferSize = 256;
+    private:
+        Semaphore* outputItemsAvailable = new Semaphore{0};
+        Semaphore* inputItemAvailable = new Semaphore{BufferSize};
+        BoundedBuffer inputBuffer, outputBuffer;
     };
 
 } // kernel
--- src/kernel/Console.cpp
+++ src/kernel/Console.cpp
@@ -5,4 +5,38 @@
 #include "../../h/kernel/Console.h"
 
 namespace kernel {
+    Console &Console::getInstance() {
+        static Console instance;
+        return instance;
+    }
+
+    char Console::readChar() {
+        return inputBuffer.get();
+    }
+
+    void Console::writeChar(char c) {
+        outputBuffer.put(c);
+    }
+
+    sem_t Console::getSemaphore(Console::Direction direction) {
+        switch (direction) {
+            case Direction::IN: return (sem_t) inputItemAvailable;
+            case Direction::OUT: return (sem_t) outputItemsAvailable;
+            default: return nullptr;
+        }
+    }
+
+    void Console::handle() {
+        while (ConsoleController::isReadable() && !outputBuffer.empty()) {
+            auto c = outputBuffer.get();
+            ConsoleController::transmitData(c);
+            outputItemsAvailable->signal();
+        }
+        while (ConsoleController::isReadable() && !inputBuffer.full()) {
+            auto c = ConsoleController::receiveData();
+            inputBuffer.put(c);
+            inputItemAvailable->signal();
+        }
+    }
+
 } // kernel
