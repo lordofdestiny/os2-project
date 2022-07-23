d2263afd14e7af2ca5e8041cca3051e531818012
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-22 19:14:06 -0700

    Removed dead comments for interrupt masking

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -24,8 +24,6 @@ namespace kernel {
 
     void Kernel::enableInterrupts() {
         using namespace kernel::BitMasks;
-        // Disable external hardware interrupts
-//        SREGISTER_CLEAR_BITS(sie, BitMasks::sie::SEIE);
         // Enable external interrupts
         SREGISTER_SET_BITS(sstatus, sstatus::SIE);
     }
