efe3c0df61293382337939d35ff5d6db086e853a
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-23 09:35:13 -0700

    Fixed the error with the tests and extended it to test console functionality

--- tests/userMain.cpp
+++ tests/userMain.cpp
@@ -1,12 +1,13 @@
-#include "../h/syscall_c.h"
 #include "../h/kernel/ConsoleUtils.h"
+#include "../h/syscall_c.h"
 
 struct SharedData{
     int arr[2];
+    int cycles = 1;
     sem_t sem;
     bool done[3];
     SharedData() {
-        sem_open(&sem,0);
+        sem_open(&sem,1);
         arr[0] = arr[1] = 0;
         for(int i = 0; i < 3; i++){
             done[i]=false;
@@ -28,14 +29,17 @@ void userMain() {
         thread_exit();
     }, nullptr);
 
+    SharedData data;
+
     thread_t thread2;
-    thread_create(&thread2, [](void *) {
-        time_sleep(30);
-        while(true) {
+    thread_create(&thread2, [](void * ptr) {
+        time_sleep(10);
+        auto data = (SharedData*)ptr;
+        while(!data->done[0] && !data->done[1] && !data->done[2]) {
             time_sleep(5);
             printString("Thread B says hi!!!\n");
         };
-    }, nullptr);
+    }, &data);
 
     thread_t thread3;
     thread_create(&thread3, [](void *) {
@@ -47,15 +51,12 @@ void userMain() {
         }
     }, nullptr);
 
-    SharedData data;
-
     thread_t threadA;
     thread_create(&threadA,[](void* arg){
         auto data = (SharedData*) arg;
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < data->cycles; i++) {
             sem_wait(data->sem);
             data->arr[0] = i;
-            thread_dispatch();
             sem_signal(data->sem);
             thread_dispatch();
         }
@@ -65,10 +66,9 @@ void userMain() {
     thread_t threadB;
     thread_create(&threadB,[](void* arg){
         auto data = (SharedData*) arg;
-        for(int i = 3; i < 6; i++){
+        for(int i = 3; i < 3 + data->cycles; i++){
             sem_wait(data->sem);
             data->arr[1] = i;
-            thread_dispatch();
             sem_signal(data->sem);
             thread_dispatch();
         }
@@ -78,12 +78,11 @@ void userMain() {
     thread_t threadC;
     thread_create(&threadC,[](void* arg){
         auto data = (SharedData*) arg;
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < data->cycles; i++){
             sem_wait(data->sem);
             char str[5];
             str[0] = data->arr[0]+48;
             str[1] = ' ';
-            thread_dispatch();
             str[2] = data->arr[1]+48;
             str[3] = '\n';
             str[4] = '\0';
@@ -98,6 +97,23 @@ void userMain() {
     while(!data.done[0] && !data.done[1] && !data.done[2]){
         thread_dispatch();
     }
+    printString("???\n");
+    char buffer[40];
+    getString(buffer,30);
+    int num = stringToInt(buffer);
+    num += 25;
+    printString("Calculating...\n");
+    time_sleep(25);
+    printString("Result: ");
+    printInt(num,8);
+    putc('\n');
+    time_sleep(5);
 
-    time_sleep(50);
+    thread_t phantom;
+    thread_create(&phantom, [](void*arg){
+        printString("Phantom starting!");
+//        time_sleep(15);
+        printString("Phantom done!");
+    }, nullptr);
+
 }
