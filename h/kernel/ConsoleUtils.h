85259b304b570f648fab1cb7b1136b0b87f4d8b8
Branislav Djumic <djumicbranislav@outlook.com>
2022-07-23 17:54:48 -0700

    Extended console utils with function provided in test files

--- h/kernel/ConsoleUtils.h
+++ h/kernel/ConsoleUtils.h
@@ -6,11 +6,20 @@
 #define PROJECT_CONSOLEUTILS_H
 #include "../../lib/hw.h"
 
+extern "C" uint64 copy_and_swap(uint64 &lock, uint64 expected, uint64 desired);
 
 void printString(const char* text);
 
 void printHex(uint64 num);
 
 void printReg(const char* name, uint64 value);
+
+char* getString(char *buf, int max);
+
+int stringToInt(const char *s);
+
+void printInt(int xx, int base=10, int sgn=0);
+
+
 
 #endif //PROJECT_CONSOLEUTILS_H
