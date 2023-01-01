1c86fe1bbd4bdac5d57088314c8f55ea5a632eb2
lordofdestiny <djumicbranislav@outlook.com>
2023-01-01 21:42:27 +0100

    Refactored and updated the tests for main

--- src/main.cpp
+++ src/main.cpp
@@ -2,98 +2,122 @@
 #include "../h/syscall_c.h"
 #include "../h/kernel/Memory/MemoryManager.h"
 #include "../h/kernel/Memory/BuddyAllocator.h"
+#include "../h/kernel/Memory/Cache.h"
 #include "../lib/hw.h"
 #include "../h/ConsoleUtils.h"
-#include "../h/kernel/Utils/Math.h"
+#include "../h/kernel/Utils/Utils.h"
 
 extern void userMain();
+
+void printMemorySection(
+    const char* name,
+    kernel::memory::MemorySection const& section)
+{
+    // Kernel::execute(userMain);
+    printString(name);
+    printString("_START_ADDR=");
+    printAddress(section.startAddress);
+    putc('\n');
+    printString(name);
+    printString("_END_ADDR=");
+    printAddress(section.endAddress);
+    putc('\n');
+    printString("TOTAL_SIZE=");
+    printUInt64(section.size(), 16);
+    putc('\n');
+    printString("----------------------------------------\n");
+}
 
 void main()
 {
     using namespace kernel;
     Kernel::initialize();
     // Kernel::execute(userMain);
-    auto dataBounds = memory::dataSectionBounds();
-    printString("DATA_START_ADDR=");
-    printAddress(dataBounds.startAddress);
-    putc('\n');
-    printString("DATA_END_ADDR=");
-    printAddress(dataBounds.endAddress);
-    putc('\n');
-    printString("TOTAL_SIZE=");
-    printUInt64(dataBounds.size(), 16);
-    putc('\n');
-    printString("----------------------------------------\n");
-
-    auto kernelBounds = memory::kernelSectionBounds();
-    printString("KERNEL_START_ADDR=");
-    printAddress(kernelBounds.startAddress);
-    putc('\n');
-    printString("KERNEL_END_ADDR=");
-    printAddress(kernelBounds.endAddress);
-    putc('\n');
-    printString("TOTAL_SIZE=");
-    printUInt64(kernelBounds.size(), 16);
-    putc('\n');
-    printString("----------------------------------------\n");
-
-    auto heapBounds = memory::heapSectionBounds();
-    printString("HEAP_START_ADDR=");
-    printAddress(heapBounds.startAddress);
-    putc('\n');
-    printString("HEAP_END_ADDR=");
-    printAddress(heapBounds.endAddress);
-    putc('\n');
-    printString("TOTAL_SIZE=");
-    printUInt64(heapBounds.size(), 16);
-    putc('\n');
-    printString("----------------------------------------\n");
+    /* printMemorySection("DATA", memory::dataSectionBounds());
+    printMemorySection("KERNEL", memory::kernelSectionBounds());
+    printMemorySection("HEAP", memory::heapSectionBounds());
 
     auto& allocator = memory::BuddyAllocator::getInstance();
-    for (int i = 0; i < BLOCK_LISTS_SIZE; i++)
-    {
-        printInt(i);
-        printString(". ");
-        printAddress(allocator.list(i));
-        putc('\n');
-    }
 
-    int allocSize = (1 << 20);
-    // int allocSize = 20;
-    int allocs = 1;
-    auto first = allocator.allocate(allocSize);
+    int pagesToAllocate = 7;
+    auto first = allocator.allocate(pagesToAllocate);
+    int allocs = first != nullptr ? 1 : 0;
     void* last = nullptr;
     void* temp = nullptr;
-    while ((temp = allocator.allocate(allocSize)) != nullptr)
+    while ((temp = allocator.allocate(pagesToAllocate)) != nullptr)
     {
         allocs++;
         last = temp;
     }
-    printInt(allocs, 16);
+    printString("Allocations: ");
+    printInt(allocs);
     putc('\n');
-    printAddress(first);
-    putc('\n');
-    printAddress(last);
-    putc('\n');
+    printString("*************************************************************************\n");
+
+    allocator.printBlockTable();
+
     printString("*************************************************************************\n");
 
     int deallocs = 0;
-    for (auto addr = first; (uint64)addr <= (uint64)last; addr = ((char*)addr) + allocSize)
+    for (auto addr = first; addr != nullptr && (uint64)addr <= (uint64)last; addr = ((char*)addr) + (PAGE_SIZE << pagesToAllocate))
     {
         deallocs++;
-        allocator.deallocate(addr, allocSize);
+        allocator.deallocate(addr, pagesToAllocate);
     }
-    printInt(deallocs, 16);
+    printString("Deallocations: ");
+    printInt(deallocs);
     putc('\n');
-    printString("*************************************************************************\n");
 
-    for (int i = 0; i < BLOCK_LISTS_SIZE; i++)
+    allocator.printBlockTable(); */
+
+    memory::Cache allocator("Test", 24, nullptr, nullptr);
+    printString(allocator.name());
+    printString("; SIZE= ");
+    printInt(allocator.objectSize());
+    printString("; OBJECT/SLAB=");
+    printInt(allocator.objectsPerSlab());
+    putc('\n');
+    printString("*****************************************\n");
+    printString("TOTAL_SIZE=");
+    printInt(allocator.totalSize());
+    putc('\n');
+    printString("SLAB_CNT=");
+    printInt(allocator.slabCount());
+    putc('\n');
+    printString("USAGE=");
+    printInt(allocator.totalUsage());
+    putc('\n');
+    printString("*****************************************\n");
+#define ALLOCS 200
+    void* ptrs[ALLOCS];
+    for (int i = 0; i < ALLOCS; i++)
     {
-        printInt(i);
-        printString(". ");
-        printAddress(allocator.list(i));
-        putc('\n');
+        ptrs[i] = allocator.allocate();
     }
+    printString("TOTAL_SIZE=");
+    printInt(allocator.totalSize());
+    putc('\n');
+    printString("SLAB_CNT=");
+    printInt(allocator.slabCount());
+    putc('\n');
+    printString("USAGE=");
+    printInt(allocator.totalUsage());
+    putc('\n');
+    printString("*****************************************\n");
+    for (int i = 0; i < ALLOCS; i++)
+    {
+        allocator.deallocate(ptrs[i]);
+    }
+    printString("TOTAL_SIZE=");
+    printInt(allocator.totalSize());
+    putc('\n');
+    printString("SLAB_CNT=");
+    printInt(allocator.slabCount());
+    putc('\n');
+    printString("USAGE=");
+    printInt(allocator.totalUsage());
+    putc('\n');
+    printString("*****************************************\n");
 
     Kernel::finalize();
 }
