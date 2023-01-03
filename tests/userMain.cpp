2ec08faa7be6ec523b0336af19a8cd4818d1b75e
lordofdestiny <djumicbranislav@outlook.com>
2023-01-03 05:08:48 +0100

    Testing with studentMain added

--- tests/userMain.cpp
+++ tests/userMain.cpp
@@ -1,8 +1,10 @@
 #include "../h/syscall_c.h"
 #include "../h/syscall_cpp.hpp"
 #include "../h/ConsoleUtils.h"
+#include "studentMain.h"
 
 void userMain()
 {
     printString("Hello world!\n");
+    studentMain();
 }
